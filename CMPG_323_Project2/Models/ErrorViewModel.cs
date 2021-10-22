580b62dfca871eda687de2c9bed53c24c6cd2338
LennertDeBer <88327272+LennertDeBer@users.noreply.github.com>
2021-10-22 11:50:05 +0200

    Add project files.

---
+++ CMPG_323_Project2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CMPG_323_Project2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
