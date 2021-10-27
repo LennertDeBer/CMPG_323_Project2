8f424a45cf55cd1b2ac53bb0b6d5807a677141aa
LennertDeBer <88327272+LennertDeBer@users.noreply.github.com>
2021-10-27 15:26:14 +0200

    Added Photo Table
    Changed the migration to include new Photo table and on the HomeController added code to get the logged in user

--- CMPG_323_Project2/Controllers/HomeController.cs
+++ CMPG_323_Project2/Controllers/HomeController.cs
@@ -6,20 +6,36 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using CMPG_323_Project2.Areas.Identity.Data;
+using System.Security.Claims;
 
 namespace CMPG_323_Project2.Controllers
 {
     public class HomeController : Controller
     {
+   
+        private readonly UserManager<AppUser> _UserManager;
+       
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> UserManager)
         {
             _logger = logger;
+            _UserManager = UserManager;
         }
-
+       
         public IActionResult Index()
         {
+            //ViewBag.userid = _UserManager.GetUserId(HttpContext.User);
+            try
+            {
+                var usid = _UserManager.GetUserId(HttpContext.User);
+            }
+            catch(Exception e)
+            {
+
+            }
             return View();
         }
 
