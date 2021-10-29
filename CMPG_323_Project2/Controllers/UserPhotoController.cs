b33cb3697c9a6e295d0dccb07c62b1027bb3a072
armand200011 <60873120+armand200011@users.noreply.github.com>
2021-10-29 12:42:15 +0200

    Login
    Photo's now only show for the user that is currently logged in so someone can't see your photo's unless they are shared with them.

--- CMPG_323_Project2/Controllers/UserPhotoController.cs
+++ CMPG_323_Project2/Controllers/UserPhotoController.cs
@@ -14,25 +14,26 @@ namespace CMPG_323_Project2.Controllers
     public class UserPhotoController : Controller
     {
         private readonly CMPG_DBContext _DBContext;
-     //   private readonly UserManager<AppUser> _UserManager;
-        public UserPhotoController(CMPG_DBContext DBContext/*, UserManager<AppUser> UserManager*/)
+        private readonly UserManager<AppUser> _UserManager;
+        public UserPhotoController(CMPG_DBContext DBContext, UserManager<AppUser> UserManager)
         {
             _DBContext = DBContext;
-        //    _UserManager = UserManager;
+            _UserManager = UserManager;
         }
         public IActionResult Index()
         {
             
-                //&& u.Id == usid
-               // var usid = _UserManager.GetUserId(HttpContext.User);
+                
+                var usid = _UserManager.GetUserId(HttpContext.User);
                 List<AspNetUser> accountusers = _DBContext.AspNetUsers.ToList();
                 List<UserPhoto> user_image_link = _DBContext.UserPhotos.ToList();
                 List<Photo> images = _DBContext.Photos.ToList();
                 var userViewModelImages = from uil in user_image_link
                                           from u in accountusers
                                           from i in images
-                                          where uil.UserId == u.Id
-                                          || uil.RecepientUserId == u.Id
+                                          where u.Id == usid
+                                          && (uil.UserId == u.Id
+                                          || uil.RecepientUserId == u.Id)
                                           where uil.PhotoId == i.PhotoId
                                           select new UserViewModelPhoto { userVm = u, photoVm = i };
                 return View(userViewModelImages);
