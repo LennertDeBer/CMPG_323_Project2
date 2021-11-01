081a2a25301ed3cd73a47459fb1a4afd10177b8f
LennertDeBer <88327272+LennertDeBer@users.noreply.github.com>
2021-11-01 11:49:23 +0200

    Added the CRUD for Album table
    Added the controller for album model
    added the views for the album CRUD
    added the ViewModels for the album table controller of view are not added

---
+++ CMPG_323_Project2/Controllers/AlbumController.cs
@@ -0,0 +1,103 @@
+﻿using CMPG_323_Project2.Data;
+using CMPG_323_Project2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMPG_323_Project2.Controllers
+{
+  
+
+
+    public class AlbumController : Controller
+    {
+        private readonly CMPG_DBContext _DBContext;
+        public AlbumController(CMPG_DBContext DBContext)
+        {
+            _DBContext=DBContext;
+    }
+        public IActionResult Index()
+        {
+            List<Album> albums=_DBContext.Albums.ToList();
+            return View(albums);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            Album album=_DBContext.Albums.Where(p => p.AlbumId==Id).FirstOrDefault();
+            return View(album);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            Album album=_DBContext.Albums.Where(p => p.AlbumId==Id).FirstOrDefault();
+            return View(album);
+        }
+        [HttpPost]
+        public IActionResult Edit(Album album)
+        {
+            _DBContext.Attach(album);
+            _DBContext.Entry(album).State = EntityState.Modified;
+            _DBContext.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public IActionResult Create(int Id)
+        {
+            Album album =new Album();
+            return View(album);
+        }
+        [HttpPost]
+        public IActionResult Create(Album album)
+        {
+            int auid=0;
+            try
+            {
+                auid=_DBContext.Albums.Max(auId => auId.AlbumId);
+            }
+            catch (Exception e)
+            {
+                auid=1;
+            }
+
+
+            int auNo;
+            int.TryParse(auid.ToString(), out auNo);
+            if (auNo>0)
+            {
+                auNo++;
+                auid=auNo;
+            }
+
+            album.AlbumId=auid;
+            _DBContext.Attach(album);
+            _DBContext.Entry(album).State=EntityState.Added;
+            _DBContext.SaveChanges();
+
+
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            Album album=_DBContext.Albums.Where(p => p.AlbumId==Id).FirstOrDefault();
+            return View(album);
+        }
+        [HttpPost]
+        public IActionResult Delete(Album album)
+        {
+            _DBContext.Attach(album);
+            _DBContext.Entry(album).State=EntityState.Deleted;
+            _DBContext.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ CMPG_323_Project2/ViewModel/UserViewModelAlbum.cs
@@ -0,0 +1,15 @@
+﻿using CMPG_323_Project2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMPG_323_Project2.ViewModel
+{
+    public class UserViewModelAlbum
+    {
+
+        public Album albumVm { get; set; }
+        public AspNetUser userVm { get; set; }
+    }
+}
